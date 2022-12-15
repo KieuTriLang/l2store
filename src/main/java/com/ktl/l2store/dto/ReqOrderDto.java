f24271bfc67efd3eb9cafa9cd5cc2cb10fa4809d
KieuTriLang <2414lq@gmail.com>
2022-12-15 15:32:58 +0700

    update

--- src/main/java/com/ktl/l2store/dto/ReqOrderDto.java
+++ src/main/java/com/ktl/l2store/dto/ReqOrderDto.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class ReqOrderDto {
 
     @AllArgsConstructor
+    @NoArgsConstructor
     @Data
     public static class Product {
         Long id;
@@ -21,6 +22,7 @@ public class ReqOrderDto {
     }
 
     @AllArgsConstructor
+    @NoArgsConstructor
     @Data
     public static class Combo {
         Long id;
